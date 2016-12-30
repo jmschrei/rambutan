0856c444d74b2b32c1dba81d79c3725813cdfcac
Jacob Schreiber <jmschreiber91@gmail.com>
2016-12-29 23:07:08 -0800

    initial commit

--- rambutan/__init__.py
+++
@@ -1,6 +0,0 @@
-# __init__.py
-# Authors: Jacob Schreiber <jmschreiber91@gmail.com>
-
-from .layers import *
-from .model import *
-from .solvers import *
--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy as np
+
+extensions = [
+            Extension( "src.*", 
+                       [ "src/*.pyx" ], 
+                       include_dirs=[np.get_include()] )
+    ]
+
+extensions = cythonize( extensions )
 
 setup(
     name='rambutan',
-    version='0.0.2',
+    version='0.4.0',
     author='Jacob Schreiber',
-    author_email='jmschreiber91@gmail.com',
-    packages=['rambutan'],
-    url='http://pypi.python.org/pypi/rambutan/',
+    author_email='jmschr@cs.washington.edu',
+    packages=['src'],
     license='LICENSE.txt',
-    description='rambutan is a python wrapper for caffe, aiming to provide a simpler way to define, train, and evaluate models.'
+    description='Rambutan is a package for the analysis of the 3D structure of the genome.',
+    ext_modules=extensions,
+    install_requires=[
+        "cython >= 0.22.1",
+        "numpy >= 1.8.0",
+        "joblib >= 0.9.0b4",
+        "mxnet >= 0.5a3"
+    ],
 )
