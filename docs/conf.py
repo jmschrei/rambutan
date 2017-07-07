85a7c27c242a3201d309d1b4e6edafda24c6baf9
jmschrei <jmschreiber91@gmail.com>
2017-07-06 20:38:12 -0700

    ADD rtd documentation/api

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-html_theme = 'sphinx_rtd_theme'
+pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -100,7 +100,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
